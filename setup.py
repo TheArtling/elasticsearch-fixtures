353d7c535180a7e4d9cc291b0e5d261eae1c6b38
Martin Brochhaus <mbrochh@gmail.com>
2019-07-22 18:54:34 +0800

    fix: fixes wrong package name in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 install_requires = open('requirements.txt').read().splitlines()
 
 setup(
-    name='elasticsearch-fixtues',
+    name='elasticsearch-fixtures',
     version=elasticsearch_fixtures.__version__,
     packages=find_packages(),
     include_package_data=True,
